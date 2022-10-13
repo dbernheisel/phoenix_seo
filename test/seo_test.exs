77d73c02132abd403e3e628650d49e6a223ca896
David Bernheisel <david@bernheisel.com>
2022-10-12 21:57:33 -0400

    Test get/assign

--- test/seo_test.exs
+++ test/seo_test.exs
@@ -2,6 +2,33 @@ defmodule SEOTest do
   use ExUnit.Case
   doctest SEO
 
-  test "greets the world" do
+  describe "item" do
+    test "gets the item from a Plug.Conn" do
+      item = %{foo: :bar}
+      conn = %Plug.Conn{} |> SEO.assign(item)
+
+      assert ^item = SEO.get(conn)
+    end
+
+    test "gets the item from a LiveView Socket" do
+      item = %{foo: :bar}
+      socket = %Phoenix.LiveView.Socket{} |> SEO.assign(item)
+
+      assert ^item = SEO.get(socket)
+    end
+
+    test "gets the item from assigns" do
+      item = %{foo: :bar}
+      conn = %Plug.Conn{} |> SEO.assign(item)
+      socket = %Phoenix.LiveView.Socket{} |> SEO.assign(item)
+
+      assert ^item = SEO.get(conn.assigns)
+      assert ^item = SEO.get(socket.assigns)
+    end
+
+  end
+
+  describe "assign" do
+
   end
 end
