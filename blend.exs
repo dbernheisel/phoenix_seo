39407adf4352ef36b42d1ec2fa09a616b147202c
David Bernheisel <david@bernheisel.com>
2026-04-12 19:48:15 -0400

    blend with newer phoenix liveview

--- blend.exs
+++ blend.exs
@@ -1,5 +1,7 @@
 %{
   phoenix_live_view_v0_18: [{:phoenix_live_view, "~> 0.18.0"}],
   phoenix_live_view_v0_19: [{:phoenix_live_view, "~> 0.19.0"}],
-  phoenix_live_view_v0_20: [{:phoenix_live_view, "~> 0.20.0"}]
+  phoenix_live_view_v0_20: [{:phoenix_live_view, "~> 0.20.0"}],
+  phoenix_live_view_v1_0: [{:phoenix_live_view, "~> 1.0.0"}],
+  phoenix_live_view_v1_1: [{:phoenix_live_view, "~> 1.1.0"}]
 }
